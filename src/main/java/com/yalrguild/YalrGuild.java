12474fbd0252ce85b099ae65b63d51c721c67431
mahyar_playgoo <mahyar.playgo@gmail.com>
2025-11-15 15:22:03 +0330

    Main file

---
+++ src/main/java/com/yalrguild/YalrGuild.java
@@ -0,0 +1,17 @@
+package com.yalrguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YalrGuild extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
